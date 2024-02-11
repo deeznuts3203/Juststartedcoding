b0949c354cec9392e8a867db702ad8fef7250948
Priyanshu Mahala <145825432+deeznuts3203@users.noreply.github.com>
2024-02-11 10:33:33 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,17 +1,88 @@
 #include <iostream>
-#include <cmath>
-int main() {
+#include <ctime> // importing ctime to set seed for srand
+#include <cstring> // to use .lower() func
+#include <algorithm>
 
-    std::cout << "Program to find hypotenuse of right angled triangle(in cm)" << '\n';
-    double hypotenuse;
-    double side1;
-    double side2;
-    std::cout << "Enter side 1: " << '\n';
-    std::cin >> side1;
-    std::cout << "Enter side 2: " << '\n'; 
-    std::cin >> side2;
-    hypotenuse = sqrt(pow(side1, 2) + pow(side2, 2));
-    std::cout << "Hypotenuse: " << hypotenuse << "cm ";
+int main(){
+    std::cout << "******NUMBER GUESSING GAME******\n";
+    srand(time(NULL));
+    int guess;
+    int tries = 0;
+    std::string difficulty;
+    std::cout << "Choose Difficulty:(easy,medium,hard) ";
+    std::cin >> difficulty;
+    std::transform(difficulty.begin(), difficulty.end(), difficulty.begin(),
+    ::tolower);
+    if (difficulty == "easy"){ // setting difficulty to easy
+
+        std::cout << "Difficulty is set to easy(1-20)";
+        int easy_diff = (rand()%20) + 1; // makes it a max of 20 numbers
+         while (tries<10){
+            std::cout << "\nGuess the number: ";
+            std::cin >> guess;
+            if(guess == easy_diff ){
+                std::cout << "You won!";
+                
+                break;
+            }else if(guess>easy_diff){
+                std::cout << "too high\n";
+                tries+=1;
+                
+                std::cout << "You have " <<10 -tries << " remaining";
+            }else if(guess<easy_diff){
+                std::cout << "too low\n";
+                tries+=1;
+                std::cout << "You have " <<10 -tries << " tries remaining";
+            }
+
+        }
+        }else if(difficulty == "medium"){
+            std::cout << "difficulty is set to medium(1-50)";
+            int medium_diff = (rand()%50) +1;
+            while (true){
+            std::cout << "\nGuess the number: ";
+            std::cin >> guess;
+            if(guess == medium_diff ){
+                std::cout << "You won!";
+                
+                break;
+            }else if(guess>medium_diff){
+                std::cout << "too high\n";
+                tries += 1;
+                std::cout << "You have " <<10 -tries << " tries remaining";
+            }else if(guess<medium_diff){
+                std::cout << "too low\n";
+                tries += 1;
+                std::cout << "You have " <<10 -tries << " tries remaining";
+            }
+
+        }
+    }else if(difficulty == "hard"){
+        
+            std::cout << "difficulty is set to hard(1-100)";
+            int hard_diff = (rand()%100) +1;
+            while (true){
+            std::cout << "\nGuess the number: ";
+            std::cin >> guess;
+            if(guess == hard_diff ){
+                std::cout << "You won!";
+                
+                break;
+            }else if(guess>hard_diff){
+                std::cout << "too high\n";
+                tries += 1;
+                std::cout << "You have " <<10 -tries << " tries remaining";
+            }else if(guess<hard_diff){
+                std::cout << "too low\n";
+                tries += 1;
+                std::cout << "You have " <<10 -tries << " tries remaining";
+            }
+
+        }
+
+    }else{
+    std::cout << "something went wrong";
+    }
     return 0;
-
 }
+
